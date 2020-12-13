d57962d38f48bcec39474350b2bb13531328c0c1
Medvedeva <catherinemedvedeff@gmail.com>
2020-12-13 20:58:21 +0300

    module1-task3

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const DEFAULT_COMMAND = `--help`;
+
+const userArguments = process.argv.slice(2);
+const [userCommand] = userArguments;
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit();
+}
+
+Cli[userCommand].run(userArguments.slice(1));
